e6c6dd54e8b3e9a054261971c3c55030749a5f16
kynm0211 <minhky.book@gmail.com>
2021-10-19 08:07:18 +0700

    new ex

---
+++ grading.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int grade[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &grade[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int nextMultiphle = 0;
+        if (grade[i]>=38)
+        {
+            nextMultiphle = grade[i] / 5;
+            nextMultiphle = (nextMultiphle + 1)*5;
+            if (nextMultiphle-grade[i]<3)
+            {
+                grade[i] = nextMultiphle;
+            }
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d \n",grade[i]);
+    } 
+    return 0;
+}
