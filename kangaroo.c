4db0d8f621aea0499caa64637c1b9329ad8380a2
kynm0211 <minhky.book@gmail.com>
2021-10-19 09:14:19 +0700

    new ex

---
+++ kangaroo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    //There're 2 kangaroos
+    int long x1, v1, x2, v2;
+    scanf("%ld", &x1);
+    scanf("%ld", &v1);
+    scanf("%ld", &x2);
+    scanf("%ld", &v2);
+    bool check = false;
+    int v1_jump = v1;
+    int v2_jump = v2;
+    if((x1<x2 && v1 < v2) || (x1==x2 && v2 > v1) || (x1==x2 && v2 < v1)){
+        printf("NO");
+    }
+    else{
+        for (;v1<=10000000 && v2<= 10000000;)
+        {
+            if(x1+v1==x2+v2){
+                check=true;
+                break;
+            }
+            v1+=v1_jump;
+            v2+=v2_jump;
+        }
+        if(check){
+            printf("YES");
+        }
+        else {
+            printf("NO");
+        }
+    }
+    return 0;
+}
