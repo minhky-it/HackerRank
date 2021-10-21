2bb6d02e11bc46a3a75fee8ecac846685c8ab4df
kynm0211 <minhky.book@gmail.com>
2021-10-21 09:38:37 +0700

    new ex

---
+++ recursion-in-c.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+//https://www.hackerrank.com/challenges/recursion-in-c/problem
+int find_nth_term(int n, int a, int b, int c) {
+  int S[20];
+  S[0] = a, S[1]=b, S[2]=c;
+  for (int i = 3; i < n; i++)
+  {
+      S[i] = S[i-1] + S[i-2] + S[i-3];
+  }
+  return S[n-1];
+
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
