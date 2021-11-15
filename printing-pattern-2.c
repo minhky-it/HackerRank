1baed77164b24a09a9545536e24d50d995f773a1
minhky0211 <minhky.book@gmail.com>
2021-11-15 12:31:33 +0700

    new ex

---
+++ printing-pattern-2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//https://www.hackerrank.com/challenges/printing-pattern-2/problem?isFullScreen=true
+int main(){
+    int n;
+    scanf("%d",&n);
+    int Size = n*2 -1;
+    int start = 0;
+    int end = Size -1;
+    int Arr[Size][Size];
+    while(n!=0){
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                if((i==start)|| (i==end)|| (j==start)|| (j==end)){
+                    Arr[i][j] = n;
+                }
+            } 
+        }
+        start++;
+        end--;
+        n--;
+        
+    }
+    for (int i = 0; i < Size; i++)
+    {
+        for (int j = 0; j < Size; j++)
+        {
+            printf("%d ", Arr[i][j]);
+            
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
